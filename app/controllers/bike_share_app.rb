42d20bb5b3d18bf4db0d54ae87cc06ccdb7e7ce4
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-12-02 20:00:23 -0700

    add station show functionality

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -9,6 +9,11 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/new"
   end
 
+  get "/stations/:id" do
+    @station = Station.find(params[:id])
+    erb :"stations/show"
+  end
+
   post "/stations" do
     Station.create(params[:station])
     redirect "/stations"
@@ -16,6 +21,16 @@ class BikeShareApp < Sinatra::Base
 
   delete "/stations/:id" do
     Station.destroy(params[:id])
+    redirect "/stations"
+  end
+
+  get "/stations/:id/edit" do
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put "/stations/:id" do
+    Station.update(params[:station])
     redirect "/stations"
   end
 end
--- spec/features/stations/user_sees_stations_spec.rb
+++ spec/features/stations/user_sees_stations_spec.rb
@@ -20,10 +20,40 @@ describe "when a user visits /stations" do
     Station.create(name:"hi35",dock_count:23,city_id:2,installation_date:'1999-23-23')
     visit "/stations"
 
-    expect(page).to have_content("hi35")
+    expect(page).to have_content "hi35"
     click_on "Delete Station : hi35"
 
     expect(page).to have_current_path "/stations"
-    expect(page).not_to have_content("hi35")
+    expect(page).not_to have_content "hi35"
   end
+
+  it "they can update an existing station" do
+    station = Station.create(name:"hi35",dock_count:23,city_id:2,installation_date:'1999-23-23')
+    visit "/stations"
+
+    expect(page).to have_content "hi35"
+    click_on "Edit Station : hi35"
+
+    expect(page).to have_current_path "/stations/#{station.id}/edit"
+    fill_in "station[name]", :with => "Dock_1"
+    fill_in "station[dock_count]", :with => 1
+    fill_in "station[city_id]", :with => 1
+    fill_in "station[installation_date]", :with => "1000-23-19"
+    click_on "Update Station"
+    expect(page).to have_content "Dock_1"
+    expect(page).to have_current_path "/stations"
+  end
+
+  it "they can see an individual station" do
+    station = Station.create(name:"hi35",dock_count:23,city_id:2,installation_date:'1999-23-23')
+    visit "/stations"
+
+    expect(page).to have_content "hi35"
+    click_on "See Station : hi35"
+    expect(page).to have_content "hi35"
+    expect(page).to have_current_path "/stations/#{station.id}"
+
+  end
+
+
 end
