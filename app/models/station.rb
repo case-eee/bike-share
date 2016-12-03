44eec003594d10761167308c14aba72982532a1e
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-03 14:54:42 -0700

    Station field validation and first validation test case

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,15 @@
 class Station < ActiveRecord::Base
 
+  validates :name, :dock_count, :city_id, :installation_date, presence: true
+
+  def self.write(station_details)
+    self.create(name: station_details[:name],
+                lat: station_details[:lat],
+                long: station_details[:long],
+                dock_count: station_details[:dock_count],
+                city_id: station_details[:city_id],
+                installation_date: station_details[:installation_date]
+                )
+  end
+
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,9 +3,25 @@ require_relative "../spec_helper"
 describe "Station" do
   describe "Validations" do
     it "Can save if all fields are present" do
-      Station.write(name: "TestStation1", lat: 1.1, long: 1.2, dock_count: 1, city_id: 1, installation_date: "2011-11-11")
+      Station.write(name: "TestStation1",
+                    lat: 1.1,
+                    long: 1.2,
+                    dock_count: 1,
+                    city_id: 1,
+                    installation_date: "2011-11-11"
+                    )
 
       expect(Station.find(1).name).to eq("TestStation1")
+    end
+    it "Will not save if name is not populated" do
+      invalid_station = Station.new(lat: 1.1,
+                                    long: 1.2,
+                                    dock_count: 1,
+                                    city_id: 1,
+                                    installation_date: "2011-11-11"
+                                    )
+
+      expect(invalid_station).to_not be_valid
     end
   end
 end
