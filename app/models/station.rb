dfe2249ccfea9713d4531424ddb8e536cdf5c445
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-12-03 14:08:30 -0700

    test station analytics

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,37 @@
 class Station < ActiveRecord::Base
-  
+
+  def self.total_stations
+    Station.all.count
+  end
+
+  def self.average_bikes_per_station
+    Station.average :dock_count
+  end
+
+  def self.max_bikes
+    Station.maximum :dock_count
+  end
+
+  def self.station_with_max_bikes
+    Station.where(dock_count: Station.maximum(:dock_count))
+  end
+
+  def self.min_bikes
+    Station.minimum :dock_count
+  end
+
+  def self.station_with_min_bikes
+    Station.where(dock_count: Station.minimum(:dock_count))
+  end
+
 end
+
+
+# Total count of stations
+# Average bikes available per station (based on docks).
+# Most bikes available at a station.
+# Station(s) where the most bikes are available.
+# Fewest bikes available at a station.
+# Station(s) where the fewest bikes are available.
+# Most recently installed station.
+# Oldest station.
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,6 +1,65 @@
 require_relative '../spec_helper'
 
 describe "Station" do
-  describe "Stations" do
+  describe ".total_stations" do
+    it "returns the total number of stations" do
+      Station.create(name:"hi35",dock_count:23,city_id:2,installation_date:'1999-23-23')
+      Station.create(name:"david",dock_count:28,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.total_stations).to eql(2)
+    end
   end
+
+  describe ".average_bikes_per_station" do
+    it "returns the average number of bikes per station" do
+      Station.create(name:"hi35",dock_count:20,city_id:2,installation_date:'1999-23-23')
+      Station.create(name:"david",dock_count:30,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.average_bikes_per_station).to eql(25)
+    end
+  end
+
+  describe ".max_bikes" do
+    it "returns max number of biks at any station" do
+      Station.create(name:"hi35",dock_count:20,city_id:2,installation_date:'1999-23-23')
+      Station.create(name:"david",dock_count:30,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.max_bikes).to eql(30)
+    end
+  end
+
+  describe ".station_with_max_bikes" do
+    it "returns the station with the most bikes" do
+      eric = Station.create(name:"eric",dock_count:20,city_id:2,installation_date:'1999-23-23')
+      david = Station.create(name:"david",dock_count:30,city_id:3,installation_date:'1999-23-24')
+      nick = Station.create(name:"nick",dock_count:30,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.station_with_max_bikes.first.id).to eql(2)
+      expect(Station.station_with_max_bikes.count).to eql(2)
+    end
+  end
+
+  describe ".min_bikes" do
+    it "returns min number of biks at any station" do
+      Station.create(name:"hi35",dock_count:20,city_id:2,installation_date:'1999-23-23')
+      Station.create(name:"david",dock_count:30,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.min_bikes).to eql(20)
+    end
+  end
+
+  describe ".station_with_min_bikes" do
+    it "returns the station with the most bikes" do
+      eric = Station.create(name:"eric",dock_count:20,city_id:2,installation_date:'1999-23-23')
+      david = Station.create(name:"david",dock_count:20,city_id:3,installation_date:'1999-23-24')
+      nick = Station.create(name:"nick",dock_count:30,city_id:3,installation_date:'1999-23-24')
+
+      expect(Station.station_with_min_bikes.first.id).to eql(1)
+      expect(Station.station_with_min_bikes.count).to eql(2)
+    end
+  end
+
+
+
+
 end
