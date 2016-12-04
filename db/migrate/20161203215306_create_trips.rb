023906bccadd1361e6c6ff20f9930275b428d0a6
David Knott <david1k1nott@gmail.com>
2016-12-04 15:21:25 -0700

    change trip table

--- db/migrate/20161203215306_create_trips.rb
+++ db/migrate/20161203215306_create_trips.rb
@@ -1,16 +1,14 @@
 class CreateTrips < ActiveRecord::Migration[5.0]
   def change
     create_table :trips do |t|
-      t.float   :duration
-      t.text      :start_station 
-      t.float   :start_station_id
-      t.text      :end_station
-      t.float   :end_station_id
+      t.float     :duration
+      t.integer   :start_station_id
+      t.integer   :end_station_id
       t.datetime  :start_date    
       t.datetime  :end_date
-      t.float   :bike_id
+      t.integer   :bike_id
       t.text      :subscription_type
-      t.float   :zip_code
+      t.integer   :zip_code
 
       t.timestamps  null:false
     end
