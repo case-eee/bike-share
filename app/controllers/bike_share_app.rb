4d1454abcb1c6b60af7b4b34d39756a516b8fe05
Marisa Burton <marisamburton@gmail.com>
2016-12-04 19:52:57 -0700

    updates CRUD

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,39 @@
 class BikeShareApp < Sinatra::Base
 
+    get '/stations' do
+      @stations = Station.all
+      erb :stations/index
+    end
+
+    get '/stations/:id' do
+      @station = Station.find(id)
+      erb :stations/show
+    end
+
+    get '/stations/new' do
+      erb :"stations/new"
+    end
+
+    post '/stations' do
+      station = Station.create(params[:stations])
+      redirect '/stations'
+    end
+
+    get '/stations/:id/edit' do
+      @station = Station.find(id)
+      erb :stations/edit
+    end
+
+    put '/stations/:id' do
+      station = Station.find(id)
+      station.update(params[:stations])
+      redirect "stations/#{station.id}"
+    end
+
+    delete '/stations/:id' do
+      station = Station.find(id)
+      station.delete(station.id)
+      redirect "stations/#{station.id}"
+    end
+
 end
