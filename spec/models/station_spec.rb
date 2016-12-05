8e78df058e1f9c6a17b8432eaf60f368e8e2e275
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-04 22:09:25 -0700

    Add test cases for Station model validation

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -23,6 +23,36 @@ describe "Station" do
 
       expect(invalid_station).to_not be_valid
     end
+    it "Will not save if dock_count is not populated" do
+      invalid_station = Station.new(name: "TestStation1",
+                                    lat: 1.1,
+                                    long: 1.2,
+                                    city_id: 1,
+                                    installation_date: "2011-11-11"
+                                    )
+
+      expect(invalid_station).to_not be_valid
+    end
+    it "Will not save if city_id is not populated" do
+      invalid_station = Station.new(name: "TestStation1",
+                                    lat: 1.1,
+                                    long: 1.2,
+                                    dock_count: 1,
+                                    installation_date: "2011-11-11"
+                                    )
+
+      expect(invalid_station).to_not be_valid
+    end
+    it "Will not save if installation_date is not populated" do
+      invalid_station = Station.new(name: "TestStation1",
+                                    lat: 1.1,
+                                    long: 1.2,
+                                    dock_count: 1,
+                                    city_id: 1
+                                    )
+
+      expect(invalid_station).to_not be_valid
+    end
     it "Can save station with correct city_id from list of multiple cities" do
       first_city = City.write(name: "FirstCity")
       second_city = City.write(name: "SecondCity")
