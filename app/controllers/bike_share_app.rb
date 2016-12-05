6c2b5a1827a843bbb52494821a11edad959fb9ba
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-04 20:34:29 -0700

    Two routes /stations and /stations/new render

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -2,16 +2,16 @@ class BikeShareApp < Sinatra::Base
 
     get '/stations' do
       @stations = Station.all
-      erb :stations/index
-    end
-
-    get '/stations/:id' do
-      @station = Station.find(id)
-      erb :stations/show
+      erb :"stations/index"
     end
 
     get '/stations/new' do
       erb :"stations/new"
+    end
+
+    get '/stations/:id' do
+      @station = Station.find(params["id"])
+      erb :stations/show
     end
 
     post '/stations' do
