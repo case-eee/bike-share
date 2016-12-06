f45fa52159ce0397fb367ec5d87dcd70c6df9ddb
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-06 11:29:19 -0700

    Conditions index page and adding new works with testing.

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,26 @@
 class BikeShareApp < Sinatra::Base
 
+  get "/conditions" do
+    @conditions = Condition.all
+    erb :"conditions/index"
+  end
+
+  get "/conditions/new" do
+    erb :"conditions/new"
+  end
+
+  get "/conditions/:id/edit" do
+    erb :"conditions/edit"
+  end
+
+  get "/conditions/:id" do
+    @condition = Condition.find(params["id"].to_i)
+    erb :"conditions/show"
+  end
+
+  post "/conditions" do
+    condition = Condition.write(params[:conditions])
+    redirect "/conditions/#{condition.id}"
+  end
+
 end
