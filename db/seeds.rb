86b45fc4ff4a28a76ff8680d7467f3f8d421e51c
njgheorghita <njgheorghita@email.wm.edu>
2016-12-06 13:50:19 -0700

    iteration8 analysis

---
+++ db/seeds.rb
@@ -0,0 +1,41 @@
+#require 'CSV'
+require 'smarter_csv'
+require "sinatra/activerecord"
+require './app/models/station'
+require './app/models/city'
+require './app/models/trip'
+require './app/models/condition'
+require 'pry'
+
+#create_stations
+CSV.foreach('db/csv/station.csv', :headers=> true) do |row|
+  City.create(name: row[5]) unless City.find_by(name: row[5])
+  city = City.find_by(name: row[5])
+  Station.create({name: row[1],
+                  lat: row[2],
+                  long: row[3],
+                  dock_count: row[4], 
+                  installation_date: row[6], 
+                  city_id: city.id})
+end
+
+#create_weathers
+SmarterCSV.process('db/csv/weather.csv').each do |row|
+  Condition.create(row)
+end
+
+#create_trips
+SmarterCSV.process('db/csv/trip.csv').each do |row|
+  Trip.create(duration: row[:duration],
+              start_date: row[:start_date],
+              start_station_id: row[:start_station_id],
+              end_date: row[:end_date],
+              end_station_id: row[:end_station_id],
+              bike_id: row[:bike_id],
+              subscription_type: row[:subscription_type],
+              zip_code: row[:zip_code])
+end
+
+# def format(time)
+#   DateTime.strptime(time, '%m/%d/%Y').strftime('%Y-%m-%d').to_s
+# end
