b314941b5c37341b7b12e55992cb2e1182af3cbd
Ann Wolff <annwolffe@gmail.com>
2016-12-05 19:04:26 -0700

    Complete CRUD implementation, some tests failing

--- spec/features/user_deletes_trip_spec.rb
+++ spec/features/user_deletes_trip_spec.rb
@@ -17,4 +17,21 @@ describe "When a user wants to delete a trip" do
     expect(current_path).to eq("/trips")
     expect(page).to have_content("Total Trips: 0")
   end
+
+  it "they press the delete button from the show file" do
+    trip = Trip.create(duration: 45,
+                      start_date: "2011-3-6 12:00",
+                      start_station_id: 1,
+                      end_date: "2011-3-6 12:00",
+                      end_station_id: 3,
+                      bike_id: 3,
+                      subscription_id: 1,
+                      zipcode: 80211)
+    visit "/trips/#{ trip.id }"
+    save_and_open_page
+    click_on("Delete")
+
+    expect(current_path).to eq("/trips")
+    expect(page).to have_content("Total Trips: 0")
+  end
 end
--- spec/features/user_updates_trip_spec.rb
+++ spec/features/user_updates_trip_spec.rb
@@ -17,8 +17,8 @@ describe "When a user visits the edit trip page" do
     expect(page).to have_content("Duration")
   end
 
-  it "allows them to update the form" do
-    Subscription.create(name: "Puzzle")
+  it "allows them to update the trip" do
+    Subscription.create(name: "Subcriber")
     trip = Trip.create(duration: 45,
                       start_date: "2011-3-6 12:00",
                       start_station_id: 1,
@@ -34,6 +34,29 @@ describe "When a user visits the edit trip page" do
 
     expect(current_path).to eq("/trips/#{ trip.id }")
     expect(page).to have_content("Duration: 47")
+  end
+
+  describe "from the index" do
+    it "they can update the trip" do
+      Subscription.create(name: "Subcriber")
+      trip = Trip.create(duration: 45,
+                      start_date: "2011-3-6 12:00",
+                      start_station_id: 1,
+                      end_date: "2011-3-6 12:00",
+                      end_station_id: 3,
+                      bike_id: 3,
+                      subscription_id: 1,
+                      zipcode: 80211)
+    
+      visit "/trips"
+      click_on "Update"
+    end
+  end
+
+  describe "from the show page" do
+    it "they can update the trip" do
+    
+    end
   end
 
 end
