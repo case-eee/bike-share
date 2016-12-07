9f7f953774f714eff2ddbf136a096dc302f4fe73
Ann Wolff <annwolffe@gmail.com>
2016-12-06 20:57:59 -0700

    Recreate station to fix issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206232118) do
+ActiveRecord::Schema.define(version: 20161207035352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
