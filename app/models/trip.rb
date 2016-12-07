2f6fcdeff051fff5b4e1ba9dd483e480cc25bebb
Ann Wolff <annwolffe@gmail.com>
2016-12-06 20:18:36 -0700

    Update station.write

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -15,8 +15,8 @@ class Trip < ActiveRecord::Base
     self.find_or_create_by(subscription_id: find_subscription_id(trip_details[:subscription_name]),
                           duration: trip_details[:duration],
                           start_date: trip_details[:start_date],
-                          start_station_id: find_station_id(trip_details[:start_station_id]),
-                          end_station_id: find_station_id(trip_details[:end_station_id]),
+                          start_station_id: find_station_id(trip_details[:start_station_name]),
+                          end_station_id: find_station_id(trip_details[:end_station_name]),
                           end_date: trip_details[:end_date],
                           bike_id: trip_details[:bike_id],
                           zipcode: trip_details[:zipcode])
@@ -27,6 +27,7 @@ class Trip < ActiveRecord::Base
   end
 
   def self.find_station_id(station_name)
+    return "This station has not been added to the database yet" if Station.find_by(name: station_name).nil?
     Station.find_by(name: station_name).id
   end
 
