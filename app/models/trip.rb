99a68841c3b9ba78ebf78c7ee2e643dafa45c0d4
njgheorghita <njgheorghita@email.wm.edu>
2016-12-07 22:30:33 -0700

    tidying up the views

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ActiveRecord::Base
-#validates :duration, :start_station_id, :end_station_id, :start_date, :end_date, :bike_id, :subscription_type, :zip_code, presence: true
+  validates :duration, :start_station_id, :end_station_id, :start_date, :end_date, :bike_id, :subscription_type, :zip_code, presence: true
   belongs_to :trip_start, :class_name => 'Station', :foreign_key => 'start_station_id'
   belongs_to :trip_end,   :class_name => 'Station', :foreign_key => 'end_station_id'
   belongs_to :condition,  :foreign_key => 'start_date'
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -100,15 +100,15 @@ describe "Trip" do
   # end
 
 
-  # describe ".average_trip_duration" do
-  #   it "returns the average trip duration" do
-  #     Trip.create(duration: 20, start_station_id: 14242, end_station_id: 13233, start_date: "11-3-2014 14:44", end_date: "11-3-2014 14:55", bike_id: 23, subscription_type: "Subscriber", zip_code: 1029)
-  #     Trip.create(duration: 40, start_station_id: 14242, end_station_id: 13233, start_date: "11-3-2014 14:44", end_date: "11-3-2014 14:55", bike_id: 23, subscription_type: "Subscriber", zip_code: 1029)
+  describe ".average_trip_duration" do
+    it "returns the average trip duration" do
+      Trip.create(duration: 20, start_station_id: 14242, end_station_id: 13233, start_date: "11-3-2014 14:44", end_date: "11-3-2014 14:55", bike_id: 23, subscription_type: "Subscriber", zip_code: 1029)
+      Trip.create(duration: 40, start_station_id: 14242, end_station_id: 13233, start_date: "11-3-2014 14:44", end_date: "11-3-2014 14:55", bike_id: 23, subscription_type: "Subscriber", zip_code: 1029)
 
 
-  #     expect(Trip.average_trip_duration).to eql(30)
-  #   end
-  # end
+      expect(Trip.average_trip_duration).to eql(30)
+    end
+  end
 
   describe ".longest_ride" do
     it "returns the longest ride out of all trips" do
