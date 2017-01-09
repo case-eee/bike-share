30cd701b3ca820b24bae7904d843a8775526598a
njgheorghita <njgheorghita@email.wm.edu>
2017-01-09 09:59:56 -0700

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@ require './app/models/condition'
 require 'pry'
 
 #create_stations
+puts 'seed file initiate'
+
 CSV.foreach('db/csv/station.csv', :headers=> true) do |row|
+  puts 'row: #{row}'
   city = City.find_or_create_by(name: row[5])
   installation_date = Date.strptime(row[6], '%m/%d/%Y')
   Station.create({id: row[0],
