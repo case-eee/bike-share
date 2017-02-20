8e86885a0b1ba6050b80955dbc3a156a0cf6dec4
njgheorghita <njgheorghita@email.wm.edu>
2017-02-19 18:27:21 -0700

    config database

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 
   # set the database based on the current environment
-  database_name = "bike-share-#{BikeShareApp.environment}"
+  database_name = "bikeshareanalytics"
   db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 
   # connect ActiveRecord with the current database
