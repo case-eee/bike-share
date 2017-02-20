f850fd522a8afe8487b1f2c2c8f092e03d2a6830
njgheorghita <njgheorghita@email.wm.edu>
2017-02-19 18:12:11 -0700

    config database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,16 @@
-# set the database based on the current environment
-database_name = "bike-share-#{BikeShareApp.environment}"
-db = URI.parse(ENV['DATABASE_URL'])
+configure :production, :development do 
+  # set the database based on the current environment
+  database_name = "bike-share-#{BikeShareApp.environment}"
+  db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 
-# connect ActiveRecord with the current database
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :port     => db.port,
-  :username => db.user,
-  :password => db.password,
-  :database => "#{database_name}",
-  :encoding => 'utf8'
-)
+  # connect ActiveRecord with the current database
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
