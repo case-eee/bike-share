6c32568ae9807e5350a2739b32db4d99219b790b
njgheorghita <njgheorghita@email.wm.edu>
2017-02-19 18:32:05 -0700

    config db

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 
   # set the database based on the current environment
   database_name = "postgresql-deep-63648"
-  db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+  db = URI.parse("postgres://yvtuqxnktvqeyw:7dc27b5b24fcc5d929df8e811cb5493828308c4c8ed2c54da9a78df86b4ae8c2@ec2-23-21-224-106.compute-1.amazonaws.com:5432/d6k1mjdukb042q")
 
   # connect ActiveRecord with the current database
   ActiveRecord::Base.establish_connection(
@@ -10,6 +10,6 @@
     :port     => db.port,
     :username => db.user,
     :password => db.password,
-    :database => "#{database_name}",
+    :database => "d6k1mjdukb042q",
     :encoding => 'utf8'
   )
